51ce47b16b52e2b6df836f2b8aa505efa477cfb8
Dmitry Ermakovich <dmitry.ermakovich@testit.software>
2026-02-06 15:25:03 +0300

    feat: cloud support

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-VERSION=0.5.0-tms.5.6
-API_CLIENT_VERSION=0.4.0-tms.5.6
+VERSION=0.5.0
+API_CLIENT_VERSION=0.5.0
 
 # Update testit-api-client version in Package.swift using sed
 sed -i "s/\.exact(\"[^\"]*\")/.exact(\"$API_CLIENT_VERSION\")/g" Package.swift
