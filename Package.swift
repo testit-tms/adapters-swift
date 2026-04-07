76ff5a6a2e1145b990583fe3fcbda0e55282e94d
Dmitry Ermakovich <dmitry.ermakovich@testit.software>
2026-04-07 13:04:13 +0300

    feat: support TMS-5.7

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/testit-tms/api-client-swift", .exact("0.5.3"))
+        .package(url: "https://github.com/testit-tms/api-client-swift", .exact("0.5.5-tms.5.7"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
